becf648013221a878bed14386ce93b129bcfdd24
banco29510 <banco29510@gmail.com>
2020-10-30 16:45:24 +0100

    30/10/2020 16:45

--- generator-python/main.py
+++ generator-python/main.py
@@ -1,5 +1,23 @@
-import re, os, shutil, math
+import re, os, shutil, math, pathlib
 import sqlite3
 import git
 
+def main ():
 
+    print("Début de la génération")
+
+
+
+    dir = str(os.path.dirname(__file__)+"/score/")
+    print(dir)
+    git.Repo.clone_from("https://github.com/banco29510/score_test.git", dir)
+
+
+    if os.path.isdir(dir):
+        shutil.rmtree(dir, ignore_errors=True)
+    else:
+        os.mkdir(dir)
+
+
+if __name__ == "__main__":
+    main()
