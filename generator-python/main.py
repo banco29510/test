0776c698e11d78c0d46cda99ee928e867342ce3a
banco29510 <banco29510@gmail.com>
2020-10-30 19:37:51 +0100

    30/10/2020 19:37

--- generator-python/main.py
+++ generator-python/main.py
@@ -13,11 +13,11 @@ def main ():
     print("Début de la génération")
 
     print("Vérification de la base de données des partitions")
+
     database = str(os.path.dirname(__file__) + "/database.db")
     if os.path.isfile(database):
         conn = sqlite3.connect('database.db')
-        # Create table
-        conn.execute('''CREATE TABLE score (id text, name text, url text)''')
+        #conn.execute('''CREATE TABLE score (id text, name text, url text)''')
         conn.commit()
     else:
         conn = sqlite3.connect('database.db')
@@ -37,8 +37,16 @@ def main ():
     if os.path.isdir(dir):
         shutil.rmtree(dir, onerror=remove_readonly) # supprime le dossier
 
+    repo = git.Repo.clone_from("https://github.com/banco29510/score_test.git", dir)
 
-    git.Repo.clone_from("https://github.com/banco29510/score_test.git", dir)
+    for branch in repo.branches:
+        print("branch : "+str(branch))
+    repo.heads.master
+    #repo.commit('master')
+
+
+    #repo.git.checkout('master')
+    #a.git.checkout.master
 
     file = open("filename.md", "w+")
     file.write("---\r\n")
@@ -46,10 +54,18 @@ def main ():
     file.write("date: 2020-10-28T08:44:03+01:00\r\n")
     file.write("draft: true\r\n")
     file.write("---\r\n")
-    file.write("README.md\r\n")
+
+    print(dir + "README.md")
+    if os.path.isfile(dir+"README.md"):
+        readme = open(dir+"README.md", "w+")
+        print(readme.read())
+        file.write(readme.read()+"\r\n")
+
     file.close()
 
     print("génération du site")
+
+    os.chdir(str(os.path.dirname(__file__)+"/.."))
     os.system("hugo -D")
 
 
