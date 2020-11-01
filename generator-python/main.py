fdbe214dba4d58f7e3eff449d8d9f9e242609207
banco29510 <banco29510@gmail.com>
2020-11-01 11:31:46 +0100

    01/11/2020 11:31

--- generator-python/main.py
+++ generator-python/main.py
@@ -1,12 +1,8 @@
-import re, os, shutil, math, pathlib, stat, cmd
+#!/usr/bin/env python3
+import re, os, shutil, math, pathlib, stat, cmd, time,sys
 import sqlite3
 import git
 
-# function pour la suppression du dossier
-def remove_readonly(func, path, _):
-    "Clear the readonly bit and reattempt the removal"
-    os.chmod(path, stat.S_IWRITE)
-    func(path)
 
 def main ():
 
@@ -15,59 +11,66 @@ def main ():
     print("Vérification de la base de données des partitions")
 
     database = str(os.path.dirname(__file__) + "/database.db")
-    if os.path.isfile(database):
+    if not os.path.isfile(database):
         conn = sqlite3.connect('database.db')
-        #conn.execute('''CREATE TABLE score (id text, name text, url text)''')
+        conn.execute('''CREATE TABLE score (id INTEGER PRIMARY KEY, name text, url text)''')
         conn.commit()
     else:
         conn = sqlite3.connect('database.db')
 
-
-
+    # recuperer l'ensemble des dépots
+    depots = []
+    for row in conn.execute('''SELECT * FROM score '''):
+        print(row)
+        depots.append(row)
 
     conn.close()
 
     print("Géneration des pages")
 
+    for depot in depots:
+        print(depot[1])
+        dir = str(os.path.dirname(__file__) + "/" +depot[1]+"/")
 
-    dir = str(os.path.dirname(__file__)+"/score/")
+        if os.path.isdir(dir):
+            shutil.rmtree(dir, ignore_errors=True)  # supprime le dossier
 
+        repo = git.Repo.clone_from(depot[2], dir)
 
-    if os.path.isdir(dir):
-        shutil.rmtree(dir, onerror=remove_readonly) # supprime le dossier
+        remote_refs = repo.remote().refs
 
-    repo = git.Repo.clone_from("https://github.com/banco29510/score_test.git", dir)
+        # liste des branches du dépot
+        for ref in remote_refs:
+            if ref.name == """origin/master""":
+                repo.git.checkout(ref.name)  # changer de branche sur master
 
-    remote_refs = repo.remote().refs
+                print("liste des commits :")
+                fifty_first_commits = list(repo.iter_commits(ref.name, max_count=10))
+                print(fifty_first_commits[0])
+                repo.commit(fifty_first_commits[0]) # choisit le dernier commit
 
-    # liste des branches du dépot
-    for ref in remote_refs:
-        if ref.name == """origin/master""":
-            repo.git.checkout(ref.name)  # changer de branche sur master
+                # liste des fichiers dans le commit
+                for (_path, _stage), entry in repo.index.entries.items():
+                    print(_path)
 
-            print("liste des commits :")
-            fifty_first_commits = list(repo.iter_commits(ref.name, max_count=10))
-            print(fifty_first_commits[0])
-            repo.commit(fifty_first_commits[0]) # choisit le dernier commit
-
-            # liste des fichiers dans le commit
-            for (_path, _stage), entry in repo.index.entries.items():
-                print(_path)
-
-                file_contents = repo.git.show('{}:{}'.format(fifty_first_commits[0],
+                    file_contents = repo.git.show('{}:{}'.format(fifty_first_commits[0],
                                                              entry.path))  # recupere le contenu du fichier
 
-                if _path == "README.md":
-                    file = open("filename.md", "w+")
-                    file.write("---\r\n")
-                    file.write("title: \"My First Post\"\r\n")
-                    file.write("date: 2020-10-28T08:44:03+01:00\r\n")
-                    file.write("draft: true\r\n")
-                    file.write("---\r\n")
+                    if _path == "README.md":
+                        file = open(str(os.path.dirname(__file__)+"/../content/posts/"+str(depot[1])+".md"), "w+")
+                        file.write("---\r\n")
+                        file.write("title: \""+str(depot[1])+"\"\r\n")
+                        file.write("date: 2020-10-28T08:44:03+01:00\r\n")
+                        file.write("draft: true\r\n")
+                        file.write("---\r\n")
 
-                    file.write(file_contents + "\r\n") # ajoute le contenu dans le fichier
+                        file.write(file_contents + "\r\n") # ajoute le contenu dans le fichier
 
-                    file.close()
+                        file.close()
+
+        print("supp du dossier qui ne fonctionne pas: " + dir)
+        shutil.rmtree(dir, ignore_errors=True)  # supprime le dossier
+
 
 
     print("génération du site")
